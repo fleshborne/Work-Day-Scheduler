e2bb43e09aa1b2800307f59d419e293f88657808
Fleshborne <blakeathompson94@gmail.com>
2020-05-01 22:01:40 -0400

    got current time to display at top of the page

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+// Create time setup to show current day
+var timeSetup = moment().format('MMM Do YYYY');
+var currentDayEl = $('#currentDay');
+currentDayEl.append(timeSetup);
+
+
+console.log(timeSetup)
+// create time block
+var timeBlock9 = $("<div>").text
+// create time block input
+// create classes for each time block to indicate past present or future
+// allow for input in timeblock fields
+// aloow for input to be saved to local storage
+// Items should perists after refresh
